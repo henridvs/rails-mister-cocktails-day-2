d204797235e4a5cfc94da844df7d9e4e03702163
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-11-22 19:28:15 +0100

    Add dose creation & deletion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # A user can see the list of cocktails
-  resources :cocktails, only: [:index, :new, :create]
+  resources :cocktails, only: [:index, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: :destroy
 end
